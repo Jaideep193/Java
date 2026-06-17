852d308cdae93656f45b674f378a4ca32c8d0827
Jaideep M C <jaideepmc2003@gmail.com>
2026-06-17 19:26:30 +0530

    improve: add Javadoc, ECOM_DATA_PATH env override, and cause-chaining in FileDatabaseLayer

--- src/com/jaideep/ecommerce/persistence/FileDatabaseLayer.java
+++ src/com/jaideep/ecommerce/persistence/FileDatabaseLayer.java
@@ -9,45 +9,56 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+/**
+ * File-based implementation of {@link DatabaseLayer} that serialises
+     * the entire {@link AppData} object graph to a single binary file.
+     *
+     * <p>The data file path can be overridden at start-up by setting the
+     * {@code ECOM_DATA_PATH} environment variable; it defaults to
+     * {@code data/ecommerce.ser}.</p>
+     */
 public class FileDatabaseLayer implements DatabaseLayer {
-    private static final String DATA_PATH = "data/ecommerce.ser";
+
+    private static final String DEFAULT_DATA_PATH = "data/ecommerce.ser";
+        static final String DATA_PATH =
+                System.getenv("ECOM_DATA_PATH") != null
+                        ? System.getenv("ECOM_DATA_PATH")
+                        : DEFAULT_DATA_PATH;
+
     private static final FileDatabaseLayer INSTANCE = new FileDatabaseLayer();
 
-    private FileDatabaseLayer() {
-    }
+    private FileDatabaseLayer() {}
 
     public static FileDatabaseLayer getInstance() {
-        return INSTANCE;
+                return INSTANCE;
     }
 
     @Override
-    public AppData load() {
-        File file = new File(DATA_PATH);
-        if (!file.exists()) {
-            return new AppData();
+        public AppData load() {
+                    File file = new File(DATA_PATH);
+                    if (!file.exists()) {
+                                    return new AppData();
+                    }
+                    try (ObjectInputStream objectInputStream =
+                                              new ObjectInputStream(new FileInputStream(file))) {
+                                    return (AppData) objectInputStream.readObject();
+                    } catch (IOException | ClassNotFoundException e) {
+                                    throw new EcommerceException("Unable to load persisted data: " + e.getMessage(), e);
+                    }
         }
-        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file))) {
-            Object data = objectInputStream.readObject();
-            if (data instanceof AppData appData) {
-                return appData;
-            }
-            throw new EcommerceException("Invalid data format in persistence file.");
-        } catch (IOException | ClassNotFoundException e) {
-            throw new EcommerceException("Unable to load persisted data: " + e.getMessage());
-        }
-    }
 
     @Override
-    public void save(AppData data) {
-        File file = new File(DATA_PATH);
-        File parent = file.getParentFile();
-        if (parent != null && !parent.exists() && !parent.mkdirs()) {
-            throw new EcommerceException("Unable to create data directory.");
+        public void save(AppData data) {
+                    File file = new File(DATA_PATH);
+                    File parent = file.getParentFile();
+                    if (parent != null && !parent.exists() && !parent.mkdirs()) {
+                                    throw new EcommerceException("Unable to create data directory: " + parent.getAbsolutePath());
+                    }
+                    try (ObjectOutputStream objectOutputStream =
+                                              new ObjectOutputStream(new FileOutputStream(file))) {
+                                    objectOutputStream.writeObject(data);
+                    } catch (IOException e) {
+                                    throw new EcommerceException("Unable to save data: " + e.getMessage(), e);
+                    }
         }
-        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file))) {
-            objectOutputStream.writeObject(data);
-        } catch (IOException e) {
-            throw new EcommerceException("Unable to save data: " + e.getMessage());
-        }
-    }
 }
